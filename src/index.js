036dc88c32199d5908853e381f93464cf53bee71
Chaitanya Ganesh <chaitanyaraju2000@gmail.com>
2021-03-13 19:45:01 +0530

    simple feedback form has been created

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function ReactPrint(){
+  return (
+    <div>
+      <label>Name:</label><input type='text'/><br/>
+      <label>Email:</label><input type='email'/><br/>
+      <label>Rating:</label>
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select><br/>
+      <input type='submit'/>
+    </div>
+  )
+}
+
+ReactDOM.render(<ReactPrint/>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
