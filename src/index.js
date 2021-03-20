f9f0ce6c58ebb2421feb78f1821691000c5dd62b
Chaitanya Ganesh <chaitanyaraju2000@gmail.com>
2021-03-20 20:35:17 +0530

    props-example

--- src/index.js
+++ src/index.js
@@ -1,28 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-function ReactPrint(){
+function Card(props) {
   return (
     <div>
-      <label>Name:</label><input type='text'/><br/>
-      <label>Email:</label><input type='email'/><br/>
-      <label>Rating:</label>
-      <select>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select><br/>
-      <input type='submit'/>
+      <img src={props.image} />
+      <h5>{props.name}</h5>
+      <p>{props.live}</p>
     </div>
-  )
+  );
 }
 
-ReactDOM.render(<ReactPrint/>,document.getElementById('root')
-);
+class Example extends React.Component {
+  render() {
+    const people = [
+      {
+        name: "B.R.Ambedkar",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Ambedkar_1966_stamp_of_India_%28cropped%29.jpg/100px-Ambedkar_1966_stamp_of_India_%28cropped%29.jpg",
+        live: "(1891-1956)",
+      },
+      {
+        name: "A.P.J. Abdul Kalam",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/APJ_Abdul_Kalam_2015_stamp_of_India_%28cropped%29.jpg/100px-APJ_Abdul_Kalam_2015_stamp_of_India_%28cropped%29.jpg",
+        live: "(1931-2015)",
+      },
+      {
+        name: "Sardar Vallabhai Patel",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Vallabhbhai_Patel_1975_stamp_of_India.jpg/100px-Vallabhbhai_Patel_1975_stamp_of_India.jpg",
+        live: "(1875-1950)",
+      },
+      {
+        name: "Nehru",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Jawaharlal_Nehru_stamp_%28cropped%29.jpg/100px-Jawaharlal_Nehru_stamp_%28cropped%29.jpg",
+        live: "(1889-1964)",
+      },
+      {
+        name: "Mother Teresa",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Mutter_Teresa_von_Kalkutta.jpg/100px-Mutter_Teresa_von_Kalkutta.jpg",
+        live: "(1910-1997)",
+      },
+      {
+        name: "Indira Gandhi",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Indira_Gandhi_%28on_stamp%29.jpg/100px-Indira_Gandhi_%28on_stamp%29.jpg",
+        live: "(1917-1984)",
+      },
+      {
+        name: "Sachin Tendulkar",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Sachin_Tendulkar_2013_stamp_of_India.jpg/100px-Sachin_Tendulkar_2013_stamp_of_India.jpg",
+        live: "(b. 1973)",
+      },
+    ];
+    return (
+      <div>
+        <h1>Great People of India</h1>
+        <div id="people-container">
+          {people.map((person) => {
+            return (
+              <Card
+                name={person.name}
+                image={person.image}
+                live={person.live}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Example />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
